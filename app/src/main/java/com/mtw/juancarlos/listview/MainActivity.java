c24f093b9609da9ea93593b9d4833c237c29078a
Juan Carlos <juan.gallegos@bachoco.net>
2016-10-09 15:10:50 -0500

    Lista con imagen fija

--- app/src/main/java/com/mtw/juancarlos/listview/MainActivity.java
+++ app/src/main/java/com/mtw/juancarlos/listview/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mtw.juancarlos.listview;
 
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //String[] arr = getResources().getStringArray(R.array.OS);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.list_item,
+                R.id.label,
+                getResources().getStringArray(R.array.OS));
+
+        ListView lista = (ListView) findViewById(R.id.ListView);
+        lista.setAdapter(adapter);
+
     }
 }
